af86427129614f5d41eaff4220c6caec7521a4ca
José Cabrera <deltadax18@gmail.com>
2016-07-15 18:40:36 -0600

    Fixed Retrofit

--- app/src/main/java/com/das/jospablo/littewebservice/webservice/RetroFitService.java
+++ app/src/main/java/com/das/jospablo/littewebservice/webservice/RetroFitService.java
@@ -1,6 +1,10 @@
 package com.das.jospablo.littewebservice.webservice;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by JoséPablo on 24/06/16.
@@ -8,12 +12,26 @@ import retrofit2.Retrofit;
  */
 public class RetroFitService {
 
-    public static GitHubApi getInstance(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .build();
+    public static final String BASE_URL = "https://api.github.com/";
+    private static Retrofit retrofit = null;
+    private static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build();
 
-        return retrofit.create(GitHubApi.class);
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .client(okHttpClient)
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static GitHubApi getInstance(){
+        return getClient().create(GitHubApi.class);
     }
 
 }
